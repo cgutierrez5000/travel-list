39f1dbe637539c0bf2805d63702c4807bc13f979
Carlos Gutierrez <c_e_gutierrez@yahoo.com>
2025-02-15 00:54:53 -0600

    Dived up more into components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import Forms from './Forms';
 import Logo from './Logo';
-
+import PackingList from './PackingList';
+import Stats from './Stats';
 
 export default function App() {
   const [items, setItems] = useState([]);
@@ -34,68 +35,4 @@ export default function App() {
       <Stats items={items} />
     </div>
   );
-
-  function PackingList({ items, onDeleteItem, onToggleItem, onClearList }) {
-    const [sortBy, setSortBy] = useState('input');
-
-    let sortedItems;
-
-    if (sortBy === 'input') sortedItems = items;
-
-    if (sortBy === 'description')
-      sortedItems = items.slice().sort((a, b) => a.description.localeCompare(b.description));
-
-    if (sortBy === 'packed')
-      sortedItems = items.slice().sort((a, b) => Number(a.packed) - Number(b.packed));
-
-
-    return (
-      <div className='list'>
-        <ul>
-          {sortedItems.map((item) => (
-            <Item item={item} key={item.id} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem} />
-          ))}
-        </ul>
-        <div className='actions'>
-          <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
-            <option value='input'>Sort by input order</option>
-            <option value='description'>Sort by description</option>
-            <option value='packed'>Sort by packed status</option>
-          </select>
-          <button onClick={onClearList}>Clear list</button>
-        </div>
-      </div>
-    );
-  }
-
-  function Item({ item, onDeleteItem, onToggleItem }) {
-    return (
-      <li>
-        <input type='checkbox' checked={item.packed} onChange={() => onToggleItem(item.id)} />
-        <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
-          {item.quantity} {item.description}
-        </span>
-        <button onClick={() => onDeleteItem(item.id)}>❌</button>
-      </li>
-    );
-  }
-
-  function Stats({ items }) {
-    if (!items.length)
-      return (
-        <footer className='stats'>
-          <em>🚀 Start adding some items to your packing list 🚀</em>
-        </footer>
-      );
-    const numItems = items.length;
-    const numPacked = items.filter((item) => item.packed).length;
-    const percentage = numItems ? Math.round((numPacked / numItems) * 100) : 0;
-    return (
-      <footer className='stats'>
-        <em>
-          {percentage === 100 ? 'You got everything! Ready to go ✈️' :
-            `💼 You have ${numItems} items on your list, and you already packed ${numPacked} (${percentage}%)`}</em>
-      </footer>
-    );
-  }
 }
